6b2b86a3edd1a628186a25b1c6d8e2e6b7ad6d32
elinzhang507 <elinzhang507@gmail.com>
2024-12-29 20:21:49 -0500

    added grid

---
+++ script.js
@@ -0,0 +1,5 @@
+let divContainer = document.querySelector(".div-container");
+for(let i = 0; i < 16; i++){
+    let gridDiv = document.createElement("div");
+    divContainer.appendChild(gridDiv);
+}
