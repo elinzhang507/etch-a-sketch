6bf58c325fbb5999fe5238eea7e7cf9b4869d235
elinzhang507 <elinzhang507@gmail.com>
2024-12-29 23:22:09 -0500

    added reset size of grid option

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 let divContainer = document.querySelector(".div-container");
-for(let i = 0; i < 16; i++){
-    let gridDiv = document.createElement("div");
-    divContainer.appendChild(gridDiv);
+
+drawGrid(16);
+
+function drawGrid(size) {
+    let px = 960 / size;
+    let isDown = false;
+    for(let i = 0; i < size*size; i++){
+        let gridDiv = document.createElement("div");
+        gridDiv.setAttribute("style", `width: ${px}px; height: ${px}px`);
+        gridDiv.addEventListener("mousedown", () => {
+            isDown = true;
+            gridDiv.style.backgroundColor = "lightgray";
+        });
+        gridDiv.addEventListener("mouseenter", () => {
+            if(isDown) {
+                gridDiv.style.backgroundColor = "lightgray";
+            }
+        });
+        gridDiv.addEventListener("mouseup", () => {
+            isDown = false;
+        });
+        divContainer.appendChild(gridDiv);
+    }
 }
+
+
+let resetBtn = document.querySelector("#reset-button");
+resetBtn.addEventListener("click", () => {
+    let num = prompt("Enter an integer between 1 and 100");
+    if (num != null) {
+        num = parseInt(num);
+        while(num > 100 || num < 1 || isNaN(num)) {
+            num = prompt("Try again! Enter an integer between 1 and 100");
+            num = parseInt(num);
+        }
+    }
+    
+    divContainer.replaceChildren();
+    drawGrid(num);
+
+});
